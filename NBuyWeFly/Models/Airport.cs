296be2b6f2c136833bf811855e579ba64b0c410b
mert.alptekin <mert.alptekin@neominal.com>
2021-11-10 19:57:20 +0300

    Airport Class Description

--- NBuyWeFly/Models/Airport.cs
+++ NBuyWeFly/Models/Airport.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace NBuyWeFly.Models
 {
+    /// <summary>
+    /// Uçuş bilgisi için From To Hava limanı seçimi için kullanırız
+    /// </summary>
     public class Airport
     {
         public string Name { get; private set; }
         public string ShortCode { get; private set; }
         public string CityName { get; private set; }
-
-
         public Airport(string name, string shortCode, string cityName)
         {
             Name = name;
